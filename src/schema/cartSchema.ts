1e26267cf678eaf403f8ac01d51d065329064daa
cholifah dewi23 <cholifahdewi23@gmail.com>
2026-07-22 00:25:27 +0700

    cartSchema.ts

--- src/schema/cartSchema.ts
+++ src/schema/cartSchema.ts
@@ -2,21 +2,26 @@ import { z } from "zod";
 
 export const addToCartSchema = z.object({
   type: z.enum(["PRODUCT", "CUSTOM_CAKE"]),
-  productId: z.string().cuid({ message: "productId tidak valid" }).optional(),
+
+  productId: z.string().min(1, "productId wajib diisi").optional(),
+
   productSizeId: z
     .string()
-    .cuid({ message: "productSizeId tidak valid" })
+    .min(1, "productSizeId wajib diisi")
     .optional(),
+
   customCakeId: z
     .string()
-    .cuid({ message: "customCakeId tidak valid" })
+    .min(1, "customCakeId wajib diisi")
     .optional(),
-  rasa: z.string().optional(), // snapshot varian rasa untuk produk reguler
+
+  rasa: z.string().optional(),
+
   qty: z.number().int().min(1, "quantity minimal 1").default(1),
 });
 
 export const deleteCartItemSchema = z.object({
-  cartItemId: z.string().cuid({ message: "cartItemId tidak valid" }),
+  cartItemId: z.string().min(1, "cartItemId wajib diisi"),
 });
 
 export type AddToCartInput = z.infer<typeof addToCartSchema>;
